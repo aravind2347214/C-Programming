8d383d262f823c10d4be000d2b8747e33f18c2c2
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-08-31 20:31:54 +0530

    dummy

--- Learning/dummy.c
+++ Learning/dummy.c
@@ -1,26 +1,51 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-#include<math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
 
-void main(){
-
-    int a[10];
-    int i,j;
-
-    for (i = 0; i < 10; i++)
+void main()
+{
+    char string[20];
+    char sub[10];
+    int ch;
+    printf("Enter String");
+    scanf("%s", &string);
+    printf("Enter Sub String");
+    scanf("%s", &sub);
+    printf("Enter Choice \n 1.String Compare \n 2.Check Presence \n 3.Copy String\n");
+    scanf("%d", &ch);
+    switch (ch)
     {
-        scanf("%d",&a[i]);
+    case 1:
+        char *result = strstr(string, sub);
+        if (!result)
+        {
+            printf("Not Present in String");
+        }
+        else
+        {
+            printf("%s", result);
+        }
+        break;
+    case 2:
+        if (!strcmp(string, sub))
+        {
+            printf("They are Equal");
+        }
+        else
+        {
+            printf("They are not Equal");
+        }
+        break;
+    case 3:
+        strcpy(sub, string);
+        printf("String Copied to substring is :%s", sub);
+        break;
+
+    default:
+        printf("Invalid choice");
+        break;
     }
 
-    int l= sizeof(a)/sizeof(a[0]);
-    printf("Reverese:  ");
-    for (i = l-1; i >=0 ; i--)
-    {
-        printf("%d\t",a[i]);
-    }
-    
-    
     getch();
-	
 }
