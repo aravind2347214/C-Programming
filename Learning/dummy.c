cf2b43387ad13816dcfc028a67d8d8359bb4ba9a
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-09-09 00:52:48 +0530

    b

--- Learning/dummy.c
+++ Learning/dummy.c
@@ -5,47 +5,13 @@
 
 void main()
 {
-    char string[20];
-    char sub[10];
-    int ch;
-    printf("Enter String");
-    scanf("%s", &string);
-    printf("Enter Sub String");
-    scanf("%s", &sub);
-    printf("Enter Choice \n 1.String Compare \n 2.Check Presence \n 3.Copy String\n");
-    scanf("%d", &ch);
-    switch (ch)
+    char string[5]="qwertwert";
+    char*ptr=string;
+    while (*ptr!='\0')
     {
-    case 1:
-        char *result = strstr(string, sub);
-        if (!result)
-        {
-            printf("Not Present in String");
-        }
-        else
-        {
-            printf("%s", result);
-        }
-        break;
-    case 2:
-        if (!strcmp(string, sub))
-        {
-            printf("They are Equal");
-        }
-        else
-        {
-            printf("They are not Equal");
-        }
-        break;
-    case 3:
-        strcpy(sub, string);
-        printf("String Copied to substring is :%s", sub);
-        break;
-
-    default:
-        printf("Invalid choice");
-        break;
+        printf("%c",*ptr);
+        ptr++;
     }
-
+    
     getch();
 }
